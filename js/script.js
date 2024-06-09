8e800f8029a4a8de101243fc99ff8e9aa11f2b1f
Rasu2 <rasul2@gmail.com>
2024-06-09 23:05:00 +0500

    Finishing the main page responsiveness

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+const addEventOnElem = (elems, event, callback) => {
+    elems.forEach(elem => elem.addEventListener(event, callback));
+  };
+  
+  const navbar = document.querySelector("[data-navbar]");
+  const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+  const navLinks = document.querySelectorAll("[data-nav-link]");
+  
+  const toggleNavbar = () => {
+    navbar.classList.toggle("active");
+  };
+  
+  addEventOnElem(navTogglers, "click", toggleNavbar);
+  
+  const closeNavbar = () => {
+    navbar.classList.remove("active");
+  };
+  
+  addEventOnElem(navLinks, "click", closeNavbar);
+
 document.addEventListener("DOMContentLoaded", function() {
     const detailsElements = document.querySelectorAll("details");
 
@@ -14,3 +34,5 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
