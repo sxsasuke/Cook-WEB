d060e2f1a7eb36364cda566a74c0feb5e8e27bd2
Rasu2 <rasul2@gmail.com>
2024-06-13 11:39:12 +0500

    start template of other pages

--- js/script.js
+++ js/script.js
@@ -2,37 +2,20 @@ const addEventOnElem = (elems, event, callback) => {
     elems.forEach(elem => elem.addEventListener(event, callback));
   };
   
-  const navbar = document.querySelector("[data-navbar]");
-  const navTogglers = document.querySelectorAll("[data-nav-toggler]");
-  const navLinks = document.querySelectorAll("[data-nav-link]");
-  
-  const toggleNavbar = () => {
-    navbar.classList.toggle("active");
-  };
-  
-  addEventOnElem(navTogglers, "click", toggleNavbar);
-  
-  const closeNavbar = () => {
-    navbar.classList.remove("active");
-  };
-  
-  addEventOnElem(navLinks, "click", closeNavbar);
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
 
-document.addEventListener("DOMContentLoaded", function() {
-    const detailsElements = document.querySelectorAll("details");
+const toggleNavbar = () => {
+  navbar.classList.toggle("active");
+};
 
-    detailsElements.forEach(details => {
-        details.addEventListener("toggle", function() {
-            const content = details.querySelector(".content");
+addEventOnElem(navTogglers, "click", toggleNavbar);
 
-            if (details.open) {
-                const height = content.scrollHeight;
-                content.style.maxHeight = `${height}px`;
-            } else {
-                content.style.maxHeight = '0';
-            }
-        });
-    });
-});
+const closeNavbar = () => {
+  navbar.classList.remove("active");
+};
+
+addEventOnElem(navLinks, "click", closeNavbar);
 
 
