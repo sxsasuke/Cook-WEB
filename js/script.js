de731ec403dd6a055652feae0deb0c3d09b53c9e
Rasu2 <rasul2@gmail.com>
2024-06-16 23:27:44 +0500

    finished the project

--- js/script.js
+++ js/script.js
@@ -19,3 +19,43 @@ const closeNavbar = () => {
 addEventOnElem(navLinks, "click", closeNavbar);
 
 
+// Map restaurants
+
+// Initialize the map
+var map = L.map('map').setView([47.0945, 51.9238], 14); // Coordinates for Atyrau, Kazakhstan
+
+// Add OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Add a marker
+L.marker([47.0945, 51.9238]).addTo(map)
+    .bindPopup('JIDA Restaurant')
+    .openPopup();
+
+
+var map2 = L.map('map2').setView([47.0945, 51.9238], 14); // Coordinates for Atyrau, Kazakhstan
+
+// Add OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map2);
+
+// Add a marker
+L.marker([47.0945, 51.9238]).addTo(map2)
+    .bindPopup('JIDA Restaurant')
+    .openPopup();
+
+
+var map3 = L.map('map3').setView([47.0945, 51.9238], 14); // Coordinates for Atyrau, Kazakhstan
+
+// Add OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map2);
+
+// Add a marker
+L.marker([47.0945, 51.9238]).addTo(map3)
+    .bindPopup('JIDA Restaurant')
+    .openPopup();
